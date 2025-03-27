6a16a2efbe5f81fe9a374d27e601be4d89a6c891
Mykull <michaelguillory.mg@gmail.com>
2025-03-27 12:36:52 -0700

    Movement

--- Assets/Source/PlayerMovement.cs
+++ Assets/Source/PlayerMovement.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 5.0f; // Speed of the player movement
+    public GameObject playerHead;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,20 +20,37 @@ public class PlayerMovement : MonoBehaviour
         // Move when wasd keys are pressed
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += transform.forward * Time.deltaTime;
+            transform.position += transform.forward * Time.deltaTime * speed;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position -= transform.forward * Time.deltaTime;
+            transform.position -= transform.forward * Time.deltaTime * speed;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position -= transform.right * Time.deltaTime;
+            transform.position -= transform.right * Time.deltaTime * speed;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += transform.right * Time.deltaTime;
+            transform.position += transform.right * Time.deltaTime * speed;
         }
-        
+        // Rotate when mouse is moved
+        if (Input.GetAxis("Mouse X") != 0)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
+        }
+
+        if (Input.GetAxis("Mouse Y") != 0)
+        {
+            // clamp using mathf.Clamp
+            float xRotation = playerHead.transform.localEulerAngles.x - Input.GetAxis("Mouse Y");
+            if (xRotation > 180)
+            {
+                xRotation -= 360;
+            }
+            xRotation = Mathf.Clamp(xRotation, -90, 90);
+            playerHead.transform.localEulerAngles = new Vector3(xRotation, playerHead.transform.localEulerAngles.y, playerHead.transform.localEulerAngles.z);
+
+        }
     }
 }
